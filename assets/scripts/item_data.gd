d490fe1e2a9029f252e5bb79274fa1fad27ba583
lyhottag13 <leonardo@promoevt.com>
2026-07-27 16:18:48 -0700

    Added Items

--- assets/scripts/item_data.gd
+++ assets/scripts/item_data.gd
@@ -2,12 +2,11 @@ class_name ItemData
 extends RefCounted
 
 static var item_data : Dictionary[String, Dictionary]= {
-	godot = 
-	{
+	godot = {
 		name = "godot",
 		title = "Godot",
 		description = "There's something fishy about this item...",
-		image = preload("uid://dhfqppklr1lsn"),
+		image = preload("uid://1fe1k5dfo650"),
 		dialogue = [
 			{
 				face = "happy",
@@ -15,16 +14,164 @@ static var item_data : Dictionary[String, Dictionary]= {
 			},
 		]
 	},
-	cave_explorer = {
-		name = "cave_explorer",
-		title = "Cave Explorer",
-		description = "Wacky.",
-		image = preload("uid://b156i5dkjq21g"),
+	
+	human_soul = {
+		name = "human_soul",
+		title = "Human Soul",
+		description = "Big, and glowy.",
+		image = preload("uid://dvqxo17tm6pvo"),
 		dialogue = [
-			{
-				face = "happy",
-				text = "Awesome!",
-			}
+			{ face = "happy", text = "I've had that lying around for a while.\nIt whispers sometimes, but that's about it." }
+		]
+	},
+
+	crown = {
+		name = "crown",
+		title = "Crown",
+		description = "Looks pretty neat.",
+		image = preload("uid://dynkck6t3xedr"),
+		dialogue = [
+			{ face = "happy", text = "Praise the lamb!\nHeh, just a little joke." }
+		]
+	},
+
+	cube_of_creation = {
+		name = "cube_of_creation",
+		title = "Cube of Creation",
+		description = "",
+		image = preload("uid://7ath0a3u0iku"),
+		dialogue = [
+			{ face = "happy", text = "Let's mine and craft! Let's MINECRAFT!" }
+		]
+	},
+
+	a_lost_sir = {
+		name = "a_lost_sir",
+		title = "A Lost Sir",
+		description = "",
+		image = preload("uid://f174r11c6xa7"),
+		dialogue = [
+			{ face = "happy", text = "A reliable ally! Make sure to get junk for him." }
+		]
+	},
+
+	basic_egg = {
+		name = "basic_egg",
+		title = "Basic Egg",
+		description = "",
+		image = preload("uid://bdo3ijxp287pt"),
+		dialogue = [
+			{ face = "happy", text = "Bee sure to feed it lotsa treats when it hatches!" }
+		]
+	},
+
+	companion_cube = {
+		name = "companion_cube",
+		title = "Companion Cube",
+		description = "",
+		image = preload("uid://cqx5ltgxyur01"),
+		dialogue = [
+			{ face = "happy", text = "I felt bad just leaving them back at the lab, so I brought some!" }
+		]
+	},
+
+	new_tank = {
+		name = "new_tank",
+		title = "New Tank",
+		description = "",
+		image = preload("uid://bepgylgqp4m7x"),
+		dialogue = [
+			{ face = "happy", text = "Lots of things by lots of people." }
+		]
+	},
+
+	nuka_cola = {
+		name = "nuka_cola",
+		title = "Nuka-Cola",
+		description = "",
+		image = preload("uid://crw6bpb71pm1k"),
+		dialogue = [
+			{ face = "happy", text = "Refreshing, straight from the quantum rivers of Nuka-WorldTM!" }
+		]
+	},
+
+	chester_bone = {
+		name = "chester_bone",
+		title = "Chester's Bone",
+		description = "",
+		image = preload("uid://cdj8vikxsrwny"),
+		dialogue = [
+			{ face = "happy", text = "Make sure not to slobber over everything you little rascal!" }
+		]
+	},
+
+	gathering_swarm = {
+		name = "gathering_swarm",
+		title = "Gathering Swarm",
+		description = "",
+		image = preload("uid://boamai1h67d1a"),
+		dialogue = [
+			{ face = "happy", text = "Useful little bugs, they help me find cool stuff!" }
+		]
+	},
+
+	stardrop = {
+		name = "stardrop",
+		title = "Stardrop",
+		description = "",
+		image = preload("uid://yfw0ge8ugv5r"),
+		dialogue = [
+			{ face = "happy", text = "Tastes like beautiful dreams! Try it out!" }
+		]
+	},
+
+	guppy = {
+		name = "guppy",
+		title = "Guppy",
+		description = "",
+		image = preload("uid://dinswbyoinu6i"),
+		dialogue = [
+			{ face = "happy", text = "I leave all that I own to Guppy!" }
+		]
+	},
+
+	spiffo = {
+		name = "spiffo",
+		title = "Spiffo",
+		description = "",
+		image = preload("uid://bc6hvtmee5y7f"),
+		dialogue = [
+			{ face = "happy", text = "It looks more like my brother than a raccoon." }
+		]
+	},
+
+	turron = {
+		name = "turron",
+		title = "Turron",
+		description = "",
+		image = preload("uid://cn5on3p5bet6e"),
+		dialogue = [
+			{ face = "happy", text = "We humbly thank you for your patronage." }
+		]
+	},
+
+	cloak_of_billowing = {
+		name = "cloak_of_billowing",
+		title = "Cloak of Billowing",
+		description = "",
+		image = preload("uid://de40ew6kewrse"),
+		dialogue = [
+			{ face = "happy", text = "Oh! How glamorous!" }
+		]
+	},
+
+	thorn_ring = {
+		name = "thorn_ring",
+		title = "Thorn Ring",
+		description = "",
+		image = preload("uid://dp6h3my66pup3"),
+		dialogue = [
+			{ face = "happy", text = "~" }
 		]
 	}
 }
--- assets/scripts/shop_menu.gd
+++ assets/scripts/shop_menu.gd
@@ -28,13 +28,14 @@ func _ready() -> void:
 	for item in ItemData.item_data.values():
 		var new_button = TextureButton.new()
 		new_button.size = Vector2(132, 132)
+		new_button.stretch_mode = TextureButton.STRETCH_KEEP_ASPECT_CENTERED
 		new_button.texture_normal = item.image
 		new_button.pivot_offset_ratio = Vector2(0.5, 0.5)
-		
 		var is_front_item = item == ItemData.item_data.values().front()
 		if is_front_item:
 			title.text = item.title
 			description.text = item.description
+		
 		new_button.position = Vector2(0, 0) if is_front_item else Vector2(0, 132)
 		new_button.pressed.connect(_on_shop_item_clicked.bind(item.name))
 		shop_item_buttons.append(new_button)
