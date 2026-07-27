f339f84e55e39dcc531f502bbd058a9226f71f0a
lyhottag13 <leonardo@promoevt.com>
2026-07-26 19:27:19 -0700

    Implemented Talking Sprites

--- assets/scripts/dialogue_box.gd
+++ assets/scripts/dialogue_box.gd
@@ -5,20 +5,47 @@ signal dialogue_finished
 @onready var label: Label = %Label
 @onready var text_sound_menu: AudioStreamPlayer = %TextSoundMenu
 @onready var text_sound_dialogue: AudioStreamPlayer = %TextSoundDialogue
+@onready var face_sprite: AnimatedSprite2D = %FaceSprite
+@onready var face_container: Control = %FaceContainer
+
+const MENU_FONT: FontFile = preload("uid://ca24jgg6u6rnm")
+const DIALOGUE_FONT: FontFile = preload("uid://m8wplwpahs")
 
 var skip: bool = false
 
 func show_text(text_properties: Dictionary):
 	label.text = "* "
-	for character in text_properties["text"]:
-		match text_properties.get("type"):
+	var type = text_properties.get("type")
+	var text = text_properties.get("text")
+	var face = text_properties.get("face")
+	print(face)
+	
+	if face != null:
+		show_face()
+		face_sprite.play("talking")
+	
+	
+	if label.has_theme_font_override("font"):
+		label.remove_theme_font_override("font")
+	
+	var font_to_use = DIALOGUE_FONT if type == "dialogue" else MENU_FONT
+	label.add_theme_font_override("font", font_to_use)
+	
+	for character in text:
+		match type:
 			"dialogue":
 				text_sound_dialogue.play()
 			_:
 				text_sound_menu.play()
+		
 		label.text += character
+		
 		if not skip:
 			await Utils.sleep(0.03)
+	
+	if face != null:
+		face_sprite.play("idling")
+	
 	dialogue_finished.emit()
 
 
@@ -28,3 +55,10 @@ func set_skip(enabled: bool):
 
 func clear() -> void:
 	show_text({text = ""})
+	hide_face()
+
+func hide_face() -> void:
+	face_container.hide()
+
+func show_face() -> void:
+	face_container.show()
